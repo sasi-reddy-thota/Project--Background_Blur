9e25fd111da054df0bb9482a79f5c5c38be10b80
Sasi <thotasasikumarreddy@gmail.com>
2022-02-12 23:56:35 +0530

    second

---
+++ script.js
@@ -0,0 +1,22 @@
+let loadText=document.querySelector('.load-text');
+let loadName=document.querySelector('.load-name');
+let bg=document.querySelector('.bg');
+
+let load=0;
+let stop=setInterval(blurring,30);
+
+function blurring(){
+    load++;
+
+    if(load>99)
+    clearInterval(stop);
+    loadText.innerHTML=`${load}%`;
+    loadText.style.opacity=scale(load,0,100,1,0);
+    loadName.style.opacity=scale(load,0,100,0,1);
+    bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`;                                        
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
